7ffc46c0be2a8aba51b11fd1d9e131ef742cc9ab
Dylan Neve <dylanneve1@gmail.com>
2022-03-28 14:49:01 +0100

    Going going gone

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ int _get_max (vector <int> y, int steps);
 int main () {
     // Variables
     int steps = 0;
-    int maxlength = 0;
 
     // Vector Arrays
     vector <string> x;
@@ -38,8 +37,13 @@ int main () {
 
 // Prints out data from two arrays
 void _print_bar_graph (int steps, vector <string> x, vector <int> y) {
+    // Get our max height
     int place = _get_max (y, steps);
+
+    // Kill switch
     bool stop = false;
+
+    // While stop not flipped print data out line by line
     while (!stop) {
         if (place <= 9) {
             cout << " " << place << "  |";
@@ -54,33 +58,38 @@ void _print_bar_graph (int steps, vector <string> x, vector <int> y) {
             }
         }
         cout << endl;
+        // Move down a place
         place--;
+        // We stop at 0 as no data is stored bellow that
         if (place == 0) {
+            // Flip kill switch
             stop = true;
         }
     }
+    // Print out meaningless border to make chart look nicer
     cout << "----|";
-    string border = "----+----|";
+    string b = "----+----|";
     if (steps <= 10) {
-        cout << border << endl;
+        cout << b << endl;
     } else if (steps <= 20) {
-        cout << border << border << endl;
+        cout << b << b << endl;
     } else if (steps <= 30) {
-        cout << border << border << border << endl;
+        cout << b << b << b << endl;
     } else if (steps <= 40) {
-        cout << border << border << border << border << endl;
+        cout << b << b << b << b << endl;
     } else if (steps <= 50) {
-        cout << border << border << border << border << border << endl;
+        cout << b << b << b << b << b << endl;
     } else if (steps <= 60) {
-        cout << border << border << border << border << border << border << endl;
+        cout << b << b << b << b << b << b << endl;
     } else if (steps <= 70) {
-        cout << border << border << border << border << border << border << border << endl;
+        cout << b << b << b << b << b << b << b << endl;
     }
 }
 
 // Get largest Y value
 int _get_max (vector <int> y, int steps) {
     int max = 0;
+    // Check through array one by one and find maximum
     for (int i = 0; i < steps; i++) {
         if (y[i] > max) {
             max = y[i];
