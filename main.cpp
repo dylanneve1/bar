35ae616b2366d765d33e412bab9c81f66876bdca
Dylan Neve <dylanneve1@gmail.com>
2022-03-28 14:12:48 +0100

    Dump from Replit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,95 @@
+// Inclusions
 #include <iostream>
+#include <iomanip>
+#include <string.h>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+// Namespace
+using namespace std;
+
+// Functions
+void _print_bar_graph (int steps, vector <string> x, vector <int> y);
+int _get_word_spacing (vector <string> x, int steps);
+int _get_max (vector <int> y, int steps);
+
+// Main
+int main () {
+    // Variables
+    bool stop = false;
+    int steps = 0;
+    int maxlength = 0;
+
+    // Vector Arrays
+    vector <string> x;
+    vector <int> y;
+
+    // Take in chart catagories
+    cout << "Enter x [end data set with -999]:";
+    while (!stop) {
+        string tmp;
+        cin >> tmp;
+        if (tmp != "-999") {
+            x.push_back (tmp);
+            steps++;
+            if (tmp.length() > maxlength) {
+                maxlength = tmp.length();
+            }
+        } else {
+            stop = true;
+        }
+    }
+
+    // Reset our bool
+    stop = false;
+
+    // Take in chart values
+    cout << "Enter y [end data set with -999]:";
+    while (!stop) {
+        int tmp;
+        cin >> tmp;
+        if (tmp != -999) {
+            y.push_back (tmp);
+        } else {
+            stop = true;
+        }
+    }
+
+    // Print out data
+    _print_bar_graph(steps, x, y);
     return 0;
+}
+
+// Prints out data from two arrays
+void _print_bar_graph (int steps, vector <string> x, vector <int> y) {
+    int place = _get_max (y, steps);
+    bool stop = false;
+    while (!stop) {
+        for (int i = 0; i < steps; i++) {
+            if (y[i] >= place) {
+                cout << "O" << " ";
+            } else {
+                cout << " " << " ";
+            }
+        }
+        cout << endl;
+        place--;
+        if (place == 0) {
+            stop = true;
+        }
+    }
+    for (int i = 0; i < steps; i++) {
+        cout << x[i] << " ";
+    }
+    cout << endl;
+}
+
+// Get largest Y value
+int _get_max (vector <int> y, int steps) {
+    int max = 0;
+    for (int i = 0; i < steps; i++) {
+        if (y[i] > max) {
+            max = y[i];
+        }
+    }
+    return max;
 }
